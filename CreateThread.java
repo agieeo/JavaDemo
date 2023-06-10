4238d4b170a9218d10fa87fc4b552be43cba8d89
agieeo <kanamemadokaw@gmail.com>
2023-06-10 20:02:59 +0800

    add a CreateThread and add a class

--- CreateThread.java
+++ CreateThread.java
@@ -1,6 +1,11 @@
 public class CreateThread {
     public static void main(String[] args) {
-        Thread t = new Thread();
+        Thread t = new MyThread();
         t.start();//start a new thread
+    }
+}
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Start a new thread");
     }
 }
