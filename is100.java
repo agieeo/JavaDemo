9c35e7d92b05abda7782a03c2c645745ae405afe
agieeo <kanamemadokaw@gmail.com>
2023-07-18 18:36:58 +0800

    为什么输出100

---
+++ is100.java
@@ -0,0 +1,16 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class is100 {
+    public static void main(String[] args) {
+        Map<Short, String> map = new HashMap<>();
+        for(short i = 0; i <100; i++)
+        {
+            map.put(i, String.valueOf(i));
+            map.remove(i-1);
+        }
+        System.out.println(map.size());
+    }
+}
+
